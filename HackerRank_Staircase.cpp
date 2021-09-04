fbf7b582258a245c1d2ecb13c07895fb87cfd7e5
Bajikomara <88127411+Bajikomara@users.noreply.github.com>
2021-09-04 09:04:57 +0530

    Add files via upload

---
+++ HackerRank_Staircase.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'staircase' function below.
+ *
+ * The function accepts INTEGER n as parameter.
+ */
+
+void staircase(int n) {
+    // int i=0;
+    int m=0;
+    while(m<n){
+        m+=1;    
+        for(int j=(n-1);j>=(m);j--){
+            cout<<" ";
+        }
+        for(int i=1;i<=m;i++){
+            cout<<"#";
+        }
+        cout<<endl; 
+        
+    } 
+}
+
+int main()
+{
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    staircase(n);
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
